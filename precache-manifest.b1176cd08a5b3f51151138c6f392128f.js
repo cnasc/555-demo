ee131b719a9eadc81ec06f76a16081008c7fce62
Christopher Nascone <chris@nasc.one>
2020-05-08 12:05:57 -0400

    Updates

---
+++ precache-manifest.b1176cd08a5b3f51151138c6f392128f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d751e6fd837124704b3c9df05011aa86",
+    "url": "/555-demo/index.html"
+  },
+  {
+    "revision": "681d5344fe2a740e8f0f",
+    "url": "/555-demo/static/css/main.efa421c8.chunk.css"
+  },
+  {
+    "revision": "e2ce5087cf4dff5cd188",
+    "url": "/555-demo/static/js/2.a45656cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/555-demo/static/js/2.a45656cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "681d5344fe2a740e8f0f",
+    "url": "/555-demo/static/js/main.d9baf31b.chunk.js"
+  },
+  {
+    "revision": "5d83055a545873138612",
+    "url": "/555-demo/static/js/runtime-main.b62eaf2e.js"
+  },
+  {
+    "revision": "4acd570758c6d93cc2bca710d6cb9bdd",
+    "url": "/555-demo/static/media/landscape.4acd5707.png"
+  },
+  {
+    "revision": "a3f4e3fd390da424c2286c86ee572e6e",
+    "url": "/555-demo/static/media/portrait.a3f4e3fd.png"
+  }
+]);
