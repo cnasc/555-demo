8e1f548196c3e3fda916f2550c07fae5770c3a41
Christopher Nascone <chris@nasc.one>
2020-05-08 16:42:35 -0400

    Layout style updates

--- src/Conversation.js
+++ src/Conversation.js
@@ -3,12 +3,20 @@ import { Bubble } from './Bubble';
 import { FaMicrophone, FaMicrophoneSlash, FaThumbsDown, FaThumbsUp } from 'react-icons/fa';
 import './conversation.css';
 
+function Item({ children }) {
+    return (
+        <div className="conversation-item">
+            {children}
+        </div>
+    )
+}
+
 export default function Conversation({
     title,
     description,
     isMuted,
 }) {
-    
+
     const bubbles = [
         <Bubble key="you" who="you" active={!isMuted} />,
         <Bubble key="them" who="them" active={isMuted} />,
@@ -16,27 +24,35 @@ export default function Conversation({
 
     return (
         <div className="conversation-wrapper">
-            <h1>{title}</h1>
-            <p>{description}</p>
-            <div className="bubble-row">
-                {bubbles}
-            </div>
-            <div className="icon-row">
-                <div className="icon red">
-                    <FaThumbsDown />
+            <Item>
+                <div>
+                    <h1>{title}</h1>
+                    <p>{description}</p>
                 </div>
-                <div className="icon green">
-                    <FaThumbsUp />
+            </Item>
+            <Item>
+                <div className="bubble-row">
+                    {bubbles}
                 </div>
-            </div>
-            <div className="icon-row">
-                {isMuted && (
-                    <div className="disabled-icon"><FaMicrophoneSlash /></div>
-                )}
-                {!isMuted && (
-                    <div className="icon orange"><FaMicrophone /></div>
-                )}
-            </div>
+            </Item>
+            <Item>
+                <div className="icon-row">
+                    <div className="icon red">
+                        <FaThumbsDown />
+                    </div>
+                    <div className="icon green">
+                        <FaThumbsUp />
+                    </div>
+                </div>
+                <div className="icon-row">
+                    {isMuted && (
+                        <div className="disabled-icon"><FaMicrophoneSlash /></div>
+                    )}
+                    {!isMuted && (
+                        <div className="icon orange"><FaMicrophone /></div>
+                    )}
+                </div>
+            </Item>
         </div>
     )
 }
